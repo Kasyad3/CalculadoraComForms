3d848590855f7cc0394e24e817384a9ea042017f
Kasyad3 <paulo.hprosa@senacsp.edu.br>
2025-03-21 21:41:16 -0300

    Calculadora feita no Windows Forms Concluída.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -45,165 +45,219 @@
             btnSub = new Button();
             btnMult = new Button();
             btnSoma = new Button();
+            btnNegativo = new Button();
             SuspendLayout();
             // 
             // btn2
             // 
-            btn2.Location = new Point(252, 115);
+            btn2.BackColor = Color.Silver;
+            btn2.Location = new Point(252, 120);
             btn2.Name = "btn2";
             btn2.Size = new Size(57, 54);
             btn2.TabIndex = 0;
             btn2.Text = "2";
-            btn2.UseVisualStyleBackColor = true;
+            btn2.UseVisualStyleBackColor = false;
+            btn2.Click += btn2_Click;
             // 
             // txtResultado
             // 
-            txtResultado.Location = new Point(175, 68);
+            txtResultado.BackColor = Color.Chartreuse;
+            txtResultado.Font = new Font("Cooper Black", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtResultado.ForeColor = SystemColors.InactiveCaptionText;
+            txtResultado.Location = new Point(175, 62);
+            txtResultado.Multiline = true;
             txtResultado.Name = "txtResultado";
-            txtResultado.Size = new Size(211, 23);
+            txtResultado.Size = new Size(211, 52);
             txtResultado.TabIndex = 1;
             // 
             // btn1
             // 
-            btn1.Location = new Point(175, 115);
+            btn1.BackColor = Color.Silver;
+            btn1.Location = new Point(175, 120);
             btn1.Name = "btn1";
             btn1.Size = new Size(57, 54);
             btn1.TabIndex = 2;
             btn1.Text = "1";
-            btn1.UseVisualStyleBackColor = true;
+            btn1.UseVisualStyleBackColor = false;
             btn1.Click += btn1_Click;
             // 
             // btn3
             // 
-            btn3.Location = new Point(329, 115);
+            btn3.BackColor = Color.Silver;
+            btn3.Location = new Point(329, 120);
             btn3.Name = "btn3";
             btn3.Size = new Size(57, 54);
             btn3.TabIndex = 3;
             btn3.Text = "3";
-            btn3.UseVisualStyleBackColor = true;
+            btn3.UseVisualStyleBackColor = false;
+            btn3.Click += btn3_Click;
             // 
             // btn9
             // 
-            btn9.Location = new Point(329, 235);
+            btn9.BackColor = Color.Gray;
+            btn9.Location = new Point(329, 240);
             btn9.Name = "btn9";
             btn9.Size = new Size(57, 54);
             btn9.TabIndex = 6;
             btn9.Text = "9";
-            btn9.UseVisualStyleBackColor = true;
+            btn9.UseVisualStyleBackColor = false;
+            btn9.Click += btn9_Click;
             // 
             // btn7
             // 
-            btn7.Location = new Point(175, 235);
+            btn7.BackColor = Color.Gray;
+            btn7.Location = new Point(175, 240);
             btn7.Name = "btn7";
             btn7.Size = new Size(57, 54);
             btn7.TabIndex = 5;
             btn7.Text = "7";
-            btn7.UseVisualStyleBackColor = true;
+            btn7.UseVisualStyleBackColor = false;
+            btn7.Click += btn7_Click;
             // 
             // btn8
             // 
-            btn8.Location = new Point(252, 235);
+            btn8.BackColor = Color.Gray;
+            btn8.Location = new Point(252, 240);
             btn8.Name = "btn8";
             btn8.Size = new Size(57, 54);
             btn8.TabIndex = 4;
             btn8.Text = "8";
-            btn8.UseVisualStyleBackColor = true;
+            btn8.UseVisualStyleBackColor = false;
+            btn8.Click += btn8_Click;
             // 
             // btn6
             // 
-            btn6.Location = new Point(329, 175);
+            btn6.BackColor = Color.DarkGray;
+            btn6.Location = new Point(329, 180);
             btn6.Name = "btn6";
             btn6.Size = new Size(57, 54);
             btn6.TabIndex = 9;
             btn6.Text = "6";
-            btn6.UseVisualStyleBackColor = true;
+            btn6.UseVisualStyleBackColor = false;
+            btn6.Click += btn6_Click;
             // 
             // btn4
             // 
-            btn4.Location = new Point(175, 175);
+            btn4.BackColor = Color.DarkGray;
+            btn4.Location = new Point(175, 180);
             btn4.Name = "btn4";
             btn4.Size = new Size(57, 54);
             btn4.TabIndex = 8;
             btn4.Text = "4";
-            btn4.UseVisualStyleBackColor = true;
+            btn4.UseVisualStyleBackColor = false;
+            btn4.Click += btn4_Click;
             // 
             // btn5
             // 
-            btn5.Location = new Point(252, 175);
+            btn5.BackColor = Color.DarkGray;
+            btn5.Location = new Point(252, 180);
             btn5.Name = "btn5";
             btn5.Size = new Size(57, 54);
             btn5.TabIndex = 7;
             btn5.Text = "5";
-            btn5.UseVisualStyleBackColor = true;
+            btn5.UseVisualStyleBackColor = false;
+            btn5.Click += btn5_Click;
             // 
             // btnIgual
             // 
-            btnIgual.Location = new Point(329, 295);
+            btnIgual.BackColor = Color.IndianRed;
+            btnIgual.Location = new Point(329, 300);
             btnIgual.Name = "btnIgual";
-            btnIgual.Size = new Size(57, 54);
+            btnIgual.Size = new Size(135, 54);
             btnIgual.TabIndex = 12;
             btnIgual.Text = "=";
-            btnIgual.UseVisualStyleBackColor = true;
+            btnIgual.UseVisualStyleBackColor = false;
+            btnIgual.Click += btnIgual_Click;
             // 
             // btnC
             // 
-            btnC.Location = new Point(175, 295);
+            btnC.BackColor = Color.Red;
+            btnC.Location = new Point(175, 300);
             btnC.Name = "btnC";
             btnC.Size = new Size(57, 54);
             btnC.TabIndex = 11;
             btnC.Text = "C";
-            btnC.UseVisualStyleBackColor = true;
+            btnC.UseVisualStyleBackColor = false;
+            btnC.Click += btnC_Click;
             // 
             // btn0
             // 
-            btn0.Location = new Point(252, 295);
+            btn0.BackColor = Color.DimGray;
+            btn0.Location = new Point(252, 300);
             btn0.Name = "btn0";
             btn0.Size = new Size(57, 54);
             btn0.TabIndex = 10;
             btn0.Text = "0";
-            btn0.UseVisualStyleBackColor = true;
+            btn0.UseVisualStyleBackColor = false;
+            btn0.Click += btn0_Click;
             // 
             // btnDiv
             // 
-            btnDiv.Location = new Point(451, 295);
+            btnDiv.BackColor = Color.FromArgb(255, 128, 0);
+            btnDiv.Location = new Point(407, 242);
             btnDiv.Name = "btnDiv";
             btnDiv.Size = new Size(57, 54);
             btnDiv.TabIndex = 16;
             btnDiv.Text = "/";
-            btnDiv.UseVisualStyleBackColor = true;
+            btnDiv.UseVisualStyleBackColor = false;
+            btnDiv.Click += btnDiv_Click;
             // 
             // btnSub
             // 
-            btnSub.Location = new Point(451, 175);
+            btnSub.BackColor = Color.Gold;
+            btnSub.Location = new Point(407, 122);
             btnSub.Name = "btnSub";
             btnSub.Size = new Size(57, 54);
             btnSub.TabIndex = 15;
             btnSub.Text = "-";
-            btnSub.UseVisualStyleBackColor = true;
+            btnSub.UseVisualStyleBackColor = false;
+            btnSub.Click += btnSub_Click;
             // 
             // btnMult
             // 
-            btnMult.Location = new Point(451, 235);
+            btnMult.BackColor = Color.Orange;
+            btnMult.Location = new Point(407, 182);
             btnMult.Name = "btnMult";
             btnMult.Size = new Size(57, 54);
             btnMult.TabIndex = 14;
             btnMult.Text = "*";
-            btnMult.UseVisualStyleBackColor = true;
+            btnMult.UseVisualStyleBackColor = false;
+            btnMult.Click += btnMult_Click;
             // 
             // btnSoma
             // 
-            btnSoma.Location = new Point(451, 115);
+            btnSoma.BackColor = Color.FromArgb(255, 192, 128);
+            btnSoma.FlatAppearance.BorderColor = Color.Red;
+            btnSoma.Location = new Point(407, 62);
             btnSoma.Name = "btnSoma";
             btnSoma.Size = new Size(57, 54);
             btnSoma.TabIndex = 13;
             btnSoma.Text = "+";
-            btnSoma.UseVisualStyleBackColor = true;
+            btnSoma.UseVisualStyleBackColor = false;
+            btnSoma.Click += btnSoma_Click;
+            // 
+            // btnNegativo
+            // 
+            btnNegativo.BackColor = Color.Crimson;
+            btnNegativo.FlatAppearance.BorderColor = SystemColors.ActiveCaptionText;
+            btnNegativo.FlatAppearance.BorderSize = 3;
+            btnNegativo.FlatStyle = FlatStyle.Flat;
+            btnNegativo.ForeColor = SystemColors.InactiveCaptionText;
+            btnNegativo.Location = new Point(504, 62);
+            btnNegativo.Name = "btnNegativo";
+            btnNegativo.Size = new Size(84, 52);
+            btnNegativo.TabIndex = 17;
+            btnNegativo.Text = "Negativo";
+            btnNegativo.UseVisualStyleBackColor = false;
+            btnNegativo.Click += btnNegativo_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlDarkDark;
             ClientSize = new Size(800, 450);
+            Controls.Add(btnNegativo);
             Controls.Add(btnDiv);
             Controls.Add(btnSub);
             Controls.Add(btnMult);
@@ -246,5 +300,6 @@
         private Button btnSub;
         private Button btnMult;
         private Button btnSoma;
+        private Button btnNegativo;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,19 +2,273 @@ namespace CalculadoraComForms
 {
     public partial class Form1 : Form
     {
+        private bool somar = false;
+        private bool dividir = false;
+        private bool multiplicar = false;
+        private bool subtrair = false;
+
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void button6_Click(object sender, EventArgs e)
-        {
-
+            txtResultado.ReadOnly = true;
         }
 
         private void btn1_Click(object sender, EventArgs e)
         {
             txtResultado.Text += "1";
         }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text += "2";
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text += "3";
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text += "4";
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text += "5";
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text += "6";
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text += "7";
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text += "8";
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text += "9";
+        }
+
+        private void btnC_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = "";
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text += "0";
+        }
+
+        private void btnIgual_Click(object sender, EventArgs e)
+        {
+
+            if (somar)
+            {
+                List<string> aSomar = txtResultado.Text.Split("+").ToList();
+
+                double primeiroNumero = Convert.ToDouble(aSomar[0]);
+
+                if (aSomar[1] == "") {
+                    if (string.IsNullOrWhiteSpace(txtResultado.Text) || !txtResultado.Text.Contains("+") && !txtResultado.Text.Contains("-") && !txtResultado.Text.Contains("*") && !txtResultado.Text.Contains("/") || txtResultado.Text.EndsWith("+") ||
+        txtResultado.Text.EndsWith("-") ||
+        txtResultado.Text.EndsWith("*") ||
+        txtResultado.Text.EndsWith("/")) ;
+                    {
+                    }
+                    return;
+                }
+                double segundoNumero = Convert.ToDouble(aSomar[1]);
+
+                double resultado = primeiroNumero + segundoNumero;
+                txtResultado.Text = resultado.ToString();
+
+                
+            }
+
+            else if (subtrair)
+            {
+                string expression = txtResultado.Text.Replace(" -(", " +(");  // Altera para a forma "5 -(-3)" para "5 +3"
+                List<string> aSubtrair = txtResultado.Text.Split("-").ToList();
+
+                double primeiroNumero = Convert.ToDouble(aSubtrair[0]);
+                if (aSubtrair[1] == "")
+                {
+                    if (string.IsNullOrWhiteSpace(txtResultado.Text) || !txtResultado.Text.Contains("+") && !txtResultado.Text.Contains("-") && !txtResultado.Text.Contains("*") && !txtResultado.Text.Contains("/") || txtResultado.Text.EndsWith("+") ||
+        txtResultado.Text.EndsWith("-") ||
+        txtResultado.Text.EndsWith("*") ||
+        txtResultado.Text.EndsWith("/")) ;
+                    {
+                    }
+                    return;
+                }
+                double segundoNumero = Convert.ToDouble(aSubtrair[1]);
+
+                double resultado = primeiroNumero - segundoNumero;
+                txtResultado.Text = resultado.ToString();
+
+               
+            }
+            else if (dividir)
+            {
+                List<string> aDividir = txtResultado.Text.Split("/").ToList();
+
+                double primeiroNumero = Convert.ToDouble(aDividir[0]);
+                if (aDividir[1] == "")
+                {
+                    if (string.IsNullOrWhiteSpace(txtResultado.Text) || !txtResultado.Text.Contains("+") && !txtResultado.Text.Contains("-") && !txtResultado.Text.Contains("*") && !txtResultado.Text.Contains("/") || txtResultado.Text.EndsWith("+") ||
+        txtResultado.Text.EndsWith("-") ||
+        txtResultado.Text.EndsWith("*") ||
+        txtResultado.Text.EndsWith("/")) ;
+                    {
+                    }
+                    return;
+                }
+                double segundoNumero = Convert.ToDouble(aDividir[1]);
+
+                double resultado = primeiroNumero / segundoNumero;
+                txtResultado.Text = resultado.ToString();
+
+                
+            }
+
+
+            else if (multiplicar)
+            {
+                List<string> aMultiplicar = txtResultado.Text.Split("*").ToList();
+
+                double primeiroNumero = Convert.ToDouble(aMultiplicar[0]);
+                if (aMultiplicar[1] == "")
+                {
+                    if (string.IsNullOrWhiteSpace(txtResultado.Text) || !txtResultado.Text.Contains("+") && !txtResultado.Text.Contains("-") && !txtResultado.Text.Contains("*") && !txtResultado.Text.Contains("/") || txtResultado.Text.EndsWith("+") ||
+        txtResultado.Text.EndsWith("-") ||
+        txtResultado.Text.EndsWith("*") ||
+        txtResultado.Text.EndsWith("/")) ;
+                    {
+                    }
+                    return;
+                }
+                double segundoNumero = Convert.ToDouble(aMultiplicar[1]);
+
+                double resultado = primeiroNumero * segundoNumero;
+                txtResultado.Text = resultado.ToString();
+
+                
+            }
+            else if (string.IsNullOrWhiteSpace(txtResultado.Text) || !txtResultado.Text.Contains("+") && !txtResultado.Text.Contains("-") && !txtResultado.Text.Contains("*") && !txtResultado.Text.Contains("/"))
+            {
+            }
+            return;
+
+        }
+
+        private void btnSoma_Click(object sender, EventArgs e)
+        {
+
+            dividir = false;
+            subtrair = false;
+            multiplicar = false;
+            somar = true;
+
+            if (txtResultado.Text[txtResultado.Text.Length - 1] == '-'
+                || txtResultado.Text[txtResultado.Text.Length - 1] == '*'
+                || txtResultado.Text[txtResultado.Text.Length - 1] == '/')
+            {
+                txtResultado.Text = txtResultado.Text.Remove(txtResultado.Text.Length - 1);
+            }
+            if (txtResultado.Text[txtResultado.Text.Length - 1] == '+')
+                return;
+            txtResultado.Text += "+";
+
+        }
+
+        private void btnSub_Click(object sender, EventArgs e)
+        {
+            dividir = false;
+            subtrair = true;
+            multiplicar = false;
+            somar = false;
+
+            if (txtResultado.Text[txtResultado.Text.Length - 1] == '+'
+                || txtResultado.Text[txtResultado.Text.Length - 1] == '*'
+                || txtResultado.Text[txtResultado.Text.Length - 1] == '/')
+            {
+                txtResultado.Text = txtResultado.Text.Remove(txtResultado.Text.Length - 1);
+            }
+            if (txtResultado.Text[txtResultado.Text.Length - 1] == '-')
+                return;
+            txtResultado.Text += "-";
+
+        }
+
+        private void btnMult_Click(object sender, EventArgs e)
+        {
+            dividir = false;
+            subtrair = false;
+            multiplicar = true;
+            somar = false;
+
+            if (txtResultado.Text[txtResultado.Text.Length - 1] == '-'
+                || txtResultado.Text[txtResultado.Text.Length - 1] == '+'
+                || txtResultado.Text[txtResultado.Text.Length - 1] == '/')
+            {
+                txtResultado.Text = txtResultado.Text.Remove(txtResultado.Text.Length - 1);
+            }
+            if (txtResultado.Text[txtResultado.Text.Length - 1] == '*')
+                return;
+            txtResultado.Text += "*";
+            ;
+        }
+
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            dividir = true;
+            subtrair = false;
+            multiplicar = false;
+            somar = false;
+
+            if (txtResultado.Text[txtResultado.Text.Length - 1] == '-'
+                || txtResultado.Text[txtResultado.Text.Length - 1] == '*'
+                || txtResultado.Text[txtResultado.Text.Length - 1] == '+')
+            {
+                txtResultado.Text = txtResultado.Text.Remove(txtResultado.Text.Length - 1);
+            }
+            if (txtResultado.Text[txtResultado.Text.Length - 1] == '/')
+                return;
+            txtResultado.Text += "/";
+            ;
+        }
+
+        private void btnNegativo_Click(object sender, EventArgs e)
+        {
+
+            // Se o texto for vazio, não faz nada
+            if (string.IsNullOrWhiteSpace(txtResultado.Text))
+                return;
+
+            // Se o texto já começa com um sinal negativo, remove ele (tornando o número positivo)
+            if (txtResultado.Text[0] == '-')
+            {
+                txtResultado.Text = txtResultado.Text.Substring(1);
+            }
+            else
+            {
+                // Se não começa com um sinal negativo, adiciona o sinal no início do texto
+                txtResultado.Text = "-" + txtResultado.Text;
+            }
+
+        }
     }
 }
+
+
+
+
